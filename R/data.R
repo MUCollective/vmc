9a080fa5e5afa34b2c4d08a71d71fe55d56b1ddc
Guoziyang27 <gzygzy991030@yahoo.com>
2023-08-11 18:01:26 -0500

    delete manufacturer model

--- R/data.R
+++ R/data.R
@@ -7,13 +7,3 @@
 #' A brms model object in Gaussian family predicting a quantitative response
 #' variable `mpg` with `disp`, `vs`, `am` as predictors.
 "mpg_model"
-
-#' Manufacturer model
-#'
-#' An example Bayesian categorical model fitted on ggplot2::mpg dataset predicts
-#' manufacturer for auotmobiles.
-#'
-#' @format ## `manufacturer_model`
-#' A brms model object in categorical family predicting a categorical response
-#' variable `manufacturer` with `cyl` as predictors.
-"manufacturer_model"
